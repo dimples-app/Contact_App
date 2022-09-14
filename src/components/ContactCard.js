325530df715f2e5a51941155fd2ee7aaed8c44fd
ddoshi2021 <ddoshi2021@gmail.com>
2022-09-13 21:22:35 -0700

    modify card

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -4,10 +4,12 @@ function ContactCard(props) {
   const {id, name, email, phone} = props;
   return (
     <div className='item'>
+      <img className='ui avatar img' src={user} alt="user" />
       <div className='content'>
         <div className='header'>{name}</div>
         <div className='header'>{email}</div>
       </div>
+      <i className='trash alternate outline icon' style={{color: 'red', marginTop: "7px"}}></i>
     </div>
   )
 }
