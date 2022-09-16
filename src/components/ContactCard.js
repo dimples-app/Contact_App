3742cfcf51f2aa5adcc4440679439088e1b22f00
ddoshi2021 <ddoshi2021@gmail.com>
2022-09-16 15:18:28 -0700

    branch

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -8,6 +8,7 @@ function ContactCard(props) {
       <div className='content'>
         <div className='header'>{name}</div>
         <div className='header'>{email}</div>
+        <div className='header'>{phone}</div>
       </div>
       <i className='trash alternate outline icon' style={{color: 'red', marginTop: "7px"}}></i>
     </div>
